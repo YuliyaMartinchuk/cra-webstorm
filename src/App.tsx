db965fc0406ede7c0baf81ea4d4a84f9e204cfca
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-03-14 00:00:02 +0300

    develop/popup branch commit2

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
           <div>Dev branch commit2</div>
           <div>Dev branch commit3</div>
           <div>develop/popup branch commit1</div>
+          <div>develop/popup branch commit2</div>
     </div>
   );
 }
