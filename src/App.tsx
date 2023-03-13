8313bbcaef6507286caa055dbf9b3f745de986af
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-03-13 23:56:49 +0300

    develop/login branch commit1

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
           <div>Dev branch commit1</div>
           <div>Dev branch commit2</div>
           <div>Dev branch commit3</div>
+          <div>develop/login branch commit1</div>
     </div>
   );
 }
